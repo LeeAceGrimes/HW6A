64da2df34cfd6759f304915d642a9444a696e443
Codey Ace Grimes <grimes.codey.l@gmail.com>
2026-07-01 12:33:34 -0600

    Near completion of Problem 1, classes are a difficult concept when learning syntax.

---
+++ hw6A1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+class fish {
+    private:
+        string name;
+        string habitat;
+        int value;
+
+    public: // public data 
+        fish(string n, string h, int v){ // constructor to acces private data to public
+            name = n;
+            habitat = h;
+            value = v;
+        }
+
+        //getter functions in class, declared with empty parentheses followed by bracket and return 
+        string getName(){return name;}
+
+        string getHabitat(){return habitat;}
+
+        int getValue(){return value;}
+
+        //setters, format is same as member functions
+        void setName(string newName){
+            name = newName;
+        }
+        void setHabitat(string newHabitat){
+            habitat = newHabitat;
+        }
+        void setValue(int newValue){
+            value = newValue;
+        }
+
+        void describe(){ // void describe belongs in public data set, call variables directly, use getters and setters in main to pass by data.
+        cout << "Fish: " << name << endl;
+        cout << "Habitat: " << habitat << endl;
+        cout << "Value: " << value << endl;
+    }
+};
+
+
+int main() {
+    //redeclaring variables from class for user input, is there an alternative?
+    string name;
+    string habitat;
+    int value;
+
+    fish starterFish("Sunfish", "river", 30); // starting sunfish
+
+    //user input for additional fish
+    cout << "Enter a fish name: ";
+    cin >> name;
+
+    cout << "Enter it's habitat: ";
+    cin >> habitat;
+
+    cout << "Enter its value: ";
+    cin >> value;
+
+    //headers
+    cout << "--- Fish Catalog ---" << endl;
+
+    // void describe function to retrieve values using getters
+        cout << "Name: " << starterFish.getName() << endl;
+        cout << "Habitat: " << starterFish.getHabitat() << endl;
+        cout << "Value: " << starterFish.getValue() << endl;
+    
+    return 0;
+}
